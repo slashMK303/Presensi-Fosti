a9dc9cb75db2207408774139d88a2e1db6c6e221
Irfanhanifs <irfanhanifsaputra27@gmail.com>
2025-05-21 20:31:21 +0700

    as;dkqm;kwdqm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="bg-blue-600 text-white p-4 shadow-md">
+        <nav className="bg-green-300 text-white p-4 shadow-md w-full">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-xl font-bold">
                     FOSTI Presensi
                 </Link>
                 <div className="flex space-x-4">
                     <Link to="/mahasiswa" className="hover:underline">
-                        Mahasiswa
+                        Mahasiswa   
                     </Link>
                     <Link to="/event" className="hover:underline">
                         Event
